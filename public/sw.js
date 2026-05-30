1cfde5981dc5e872a5006282ce7d0e9d0b478f16
iPrashant2003 <prashantmanitripathi2003@gmail.com>
2026-05-30 15:04:55 +0530

    chore: bump service worker cache version to v2 to force icon refresh

--- public/sw.js
+++ public/sw.js
@@ -12,7 +12,7 @@ if (workbox) {
   const { ExpirationPlugin } = workbox.expiration;
 
   // Pre-cache core shell resources on install
-  const OFFLINE_CACHE_NAME = 'atul-residency-offline-v1';
+  const OFFLINE_CACHE_NAME = 'atul-residency-offline-v2';
   const PRECACHE_ASSETS = [
     '/offline',
     '/manifest.json',
